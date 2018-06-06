2925f427938b17ae0a41f492dbcdce0c753a1c75
Your Name <you@example.com>
2018-06-06 12:00:48 -0300

    improve events

---
+++ js/calcbrain.js
@@ -0,0 +1,8 @@
+var display =  document.querySelector("#displayaritimetic");
+
+if (display.textContent[0] == '0' && display.textContent[1]) {
+
+
+	display.textContent[0] = display.textContent[1];
+
+};
