ed31bfaf8017f2168faebaa0adfdd469877c8a3d
Your Name <you@example.com>
2018-06-06 15:50:38 -0300

    with SUM

--- js/calcbrain.js
+++ js/calcbrain.js
@@ -1,8 +1,25 @@
-var display =  document.querySelector("#displayaritimetic");
-
-if (display.textContent[0] == '0' && display.textContent[1]) {
 
 
-	display.textContent[0] = display.textContent[1];
 
-};
+function sum () {
+	var display =  document.querySelector("#displayaritimetic");
+	var savedata = document.querySelector("#save");
+
+	N1 = display.textContent;
+	N2 = savedata.textContent;
+	Restult = parseInt(N1) +  parseInt(N2);
+	console.log(Restult);
+	display.textContent = Restult;
+	
+	
+}
+
+function save (){
+	var display =  document.querySelector("#displayaritimetic");
+	var savedata = document.querySelector("#save");
+
+	savedata.textContent = display.textContent;
+	console.log(savedata.textContent);
+	display.textContent = "";
+
+}
