ce4445b8fad6194598bbf37be372a06b9df04642
Your Name <you@example.com>
2018-06-06 17:36:38 -0300

    appi 1.0

--- js/calcbrain.js
+++ js/calcbrain.js
@@ -1,7 +1,40 @@
 
+function validates() {
+
+	var destiny = document.querySelector("#save2");
+
+	
+
+		if(destiny.textContent == 'a'){
+
+		 sum();
+
+		}
+
+		if(destiny.textContent == 'b'){
+
+		 sube();
+
+		}
+
+		if(destiny.textContent == 'c'){
+
+		 mult();
+
+		}
+
+		if(destiny.textContent == 'd'){
+
+		 div();
+
+		}
 
 
-function sum () {
+	
+
+}
+
+function sum() {
 	var display =  document.querySelector("#displayaritimetic");
 	var savedata = document.querySelector("#save");
 
@@ -14,12 +47,56 @@ function sum () {
 	
 }
 
-function save (){
+function sube() {
 	var display =  document.querySelector("#displayaritimetic");
 	var savedata = document.querySelector("#save");
+
+	N1 = display.textContent;
+	N2 = savedata.textContent;
+	Restult = parseInt(N2) -  parseInt(N1);
+	console.log(Restult);
+	display.textContent = Restult;
+	
+	
+}
+
+function mult() {
+	var display =  document.querySelector("#displayaritimetic");
+	var savedata = document.querySelector("#save");
+
+	N1 = display.textContent;
+	N2 = savedata.textContent;
+	Restult = parseInt(N1) *  parseInt(N2);
+	console.log(Restult);
+	display.textContent = Restult;
+	
+	
+}
+
+function div() {
+	var display =  document.querySelector("#displayaritimetic");
+	var savedata = document.querySelector("#save");
+
+	N1 = display.textContent;
+	N2 = savedata.textContent;
+	Restult = parseInt(N2) /  parseInt(N1);
+	console.log(Restult);
+	display.textContent = Restult;
+	
+	
+}
+
+function save (operation){
+	var display =  document.querySelector("#displayaritimetic");
+	var savedata = document.querySelector("#save");
+	var destiny = document.querySelector("#save2");
+
+    destiny.textContent = operation;
+    console.log(destiny.textContent);
 
 	savedata.textContent = display.textContent;
 	console.log(savedata.textContent);
 	display.textContent = "";
 
 }
+
